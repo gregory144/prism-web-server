5e958349f6271122fe5db37b7bac54a14c0e662f
Greg Gross <greg@gtgross.com>
2014-04-26 16:51:08 +1000

    code style changes in circ buffer

--- src/hpack/circular_buffer.h
+++ src/hpack/circular_buffer.h
@@ -9,27 +9,28 @@ typedef struct {
   size_t length;
   size_t capacity;
   size_t shift;
-  void** entries;
+  void * * entries;
 } circular_buffer_t;
 
 typedef struct {
-  circular_buffer_t* buf;
+  const circular_buffer_t * buf;
   size_t index;
-  void* value;
+  void * value;
 } circular_buffer_iter_t;
 
-circular_buffer_t* circular_buffer_init(size_t capacity);
+circular_buffer_t * circular_buffer_init(const size_t capacity);
 
-bool circular_buffer_add(circular_buffer_t* buf, void* entry);
+bool circular_buffer_add(circular_buffer_t * const buf, void * entry);
 
-void* circular_buffer_get(circular_buffer_t* buf, size_t index);
+void * circular_buffer_get(const circular_buffer_t * const buf, const size_t index);
 
-void* circular_buffer_evict(circular_buffer_t* buf);
+void * circular_buffer_evict(circular_buffer_t * const buf);
 
-void circular_buffer_iterator_init(circular_buffer_iter_t* iter, circular_buffer_t* buf);
+void circular_buffer_iterator_init(circular_buffer_iter_t * const iter, const circular_buffer_t * const buf);
 
-bool circular_buffer_iterate(circular_buffer_iter_t* iter);
+bool circular_buffer_iterate(circular_buffer_iter_t * const iter);
 
-void circular_buffer_free(circular_buffer_t* buf, void (free_entry)(void*));
+void circular_buffer_free(circular_buffer_t * const buf, void (free_entry)(void *));
 
 #endif
+
