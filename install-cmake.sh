e07595f78836b68cf264ca47c5342a20baa865b7
Greg Gross <greg@gtgross.com>
2015-03-05 02:25:35 +1100

    Fix cmake install script. It's 2:30am - stupid mistake

--- install-cmake.sh
+++ install-cmake.sh
@@ -3,5 +3,4 @@ set -ex
 
 wget http://www.cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz
 tar -xzf cmake-3.1.3-Linux-x86_64.tar.gz
-cd cmake-3.1.3-Linux-x86_64
 mv cmake-3.1.3-Linux-x86_64 cmake
