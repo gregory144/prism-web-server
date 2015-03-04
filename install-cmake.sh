f5b99d3e440f83aa9caba09400a1081965b2c711
Greg Gross <greg@gtgross.com>
2015-03-05 02:42:33 +1100

    Install cmake to another directory so it doesn't conflict with the existing cmake directory

--- install-cmake.sh
+++ install-cmake.sh
@@ -3,4 +3,4 @@ set -ex
 
 wget http://www.cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz
 tar -xzf cmake-3.1.3-Linux-x86_64.tar.gz
-mv cmake-3.1.3-Linux-x86_64 cmake
+mv cmake-3.1.3-Linux-x86_64 cmake-bin
